99216dfc111d9d8c8adff0c9ea0b4349ad035d9a
klausqama <klaussqama@gmail.com>
2024-10-18 10:16:01 +0200

    add ex.2

--- ex.2/style.js
+++ ex.2/style.js
@@ -2,3 +2,15 @@ const name = 'Klaus'
 console.log(name)
 //Ex.2 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 //Stabilire il vincitore, in base a chi fa il punteggio più alto.
+const numeroUser = Math.floor(Math.random() * 6) + 1
+const numeroComp = Math.floor(Math.random() *6 ) + 1
+console.log(numeroComp,numeroUser)
+let message = 'pareggio'
+
+if(numeroComp>numeroUser)
+    message= 'hai perso'
+
+else(numeroComp<numeroUser)
+    message= 'hai vinto'
+
+console.log(message)
